98cc8363ba95acab2dbe80ff49b082e1c5e12731
Urvashi021 <parmarurvashi01@gmail.com>
2024-01-14 13:08:40 +0530

    Chore : Include FileReader() for Upload File.

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function vplay()
-{
-    var a = document.getElementById("vdo");
-    a.play();
+function vplay() {
+  var a = document.getElementById("vdo");
+  a.play();
 }
 function vpause() {
-        var a = document.getElementById
-            ("vdo");
-        a.pause();
-    }
- function upload()
- {
-    var a = document.getElementById("vupload").value.replace(/.*(\/|\\)/,
-'');
-   // window.alert(a);
-   var b = document.getElementById("vdo");
-   b.src=a;
- }   
+  var a = document.getElementById("vdo");
+  a.pause();
+}
+/* function upload() {
+  var a = document.getElementById("vupload").value.replace(/.*(\/|\\)/, "");
+//   window.alert(a);
+  var b = document.getElementById("vdo");
+  b.src = a;
+} */
+
+var btn = document.getElementById("vupload");
+btn.onchange = function () {
+  var reader = new FileReader();
+  reader.readAsDataURL(btn.files[0]);
+
+  reader.onload = function () {
+    var file_name = reader.result;
+    var video = document.getElementById("vdo");
+    video.src = file_name;
+  };
+};
